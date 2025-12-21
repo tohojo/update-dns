a1d0e73c5bcfb7da74b94ef643aad3526f1f803f
Toke Høiland-Jørgensen <toke@toke.dk>
2025-12-21 22:28:56 +0100

    Add command line argument and config file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use clap::{Parser, ValueEnum};
+use hickory_proto::dnssec::rdata::tsig::TsigAlgorithm;
+use proc_exit::Code;
+use serde::{Deserialize, Serialize};
+use serde_with::base64::Base64;
+use serde_with::serde_as;
+use std::fs::File;
+use tracing::{debug, error};
 
 /// A DNS record type.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -38,8 +45,49 @@ struct Args {
     ttl: u32,
 }
 
-fn main() {
-    let args = Args::parse();
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Config {
+    server: String,
+    key: TsigKey,
+}
 
-    println!("Hello, world! {:?}", args);
+#[serde_as]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct TsigKey {
+    name: String,
+    algorithm: TsigAlgorithm,
+    #[serde_as(as = "Base64")]
+    data: Vec<u8>,
+}
+
+fn main() -> proc_exit::Exit {
+    tracing_subscriber::fmt::init();
+
+    let args = Args::parse();
+    let mut config_file = dirs::config_dir().unwrap();
+    config_file.push("update-dns");
+    config_file.push("config.yml");
+
+    let fd = match File::open(config_file) {
+        Ok(file) => file,
+        Err(error) => {
+            error!("Unable to open config file: {}", error);
+            return Code::FAILURE.as_exit();
+        }
+    };
+
+    let config: Config = match serde_yaml::from_reader(fd) {
+        Ok(cfg) => cfg,
+        Err(error) => {
+            error!("Unable to parse configuration file: {}", error);
+            return Code::FAILURE.as_exit();
+        }
+    };
+    debug!(args = ?args,
+           server = config.server,
+           key.name = config.key.name,
+           key.algorithm = %config.key.algorithm,
+           "Init OK");
+
+    Code::SUCCESS.as_exit()
 }
